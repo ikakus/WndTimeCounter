6d01bfe1da7467387b6d41b0722520474f9ea065
ikakus <idadiani88@gmail.com>
2014-01-03 22:33:14 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WndTimeCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Window = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_filter = new System.Windows.Forms.TextBox();
+            this.label_timeSummary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Window,
+            this.Time});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(681, 496);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Descending;
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Window
+            // 
+            this.Window.Text = "Time";
+            this.Window.Width = 78;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Window";
+            this.Time.Width = 491;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 538);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // textBox_filter
+            // 
+            this.textBox_filter.Location = new System.Drawing.Point(116, 11);
+            this.textBox_filter.Name = "textBox_filter";
+            this.textBox_filter.Size = new System.Drawing.Size(578, 20);
+            this.textBox_filter.TabIndex = 7;
+            // 
+            // label_timeSummary
+            // 
+            this.label_timeSummary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label_timeSummary.AutoSize = true;
+            this.label_timeSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_timeSummary.Location = new System.Drawing.Point(9, 538);
+            this.label_timeSummary.Name = "label_timeSummary";
+            this.label_timeSummary.Size = new System.Drawing.Size(62, 17);
+            this.label_timeSummary.TabIndex = 6;
+            this.label_timeSummary.Text = "timeSum";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 564);
+            this.Controls.Add(this.textBox_filter);
+            this.Controls.Add(this.label_timeSummary);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Carpe Diem Time Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Window;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_filter;
+        private System.Windows.Forms.Label label_timeSummary;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,577 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WndTimeCounter
+{
+    public partial class Form1 : Form
+    {
+
+
+        delegate void UpdateLabelDelegate(String str, Label la);
+        delegate void UpdateListviewDelegate(String window, String time,ListView list);
+        delegate void UpdateFileredListviewDelegate(String window, String time, ListView list,string filter);
+        delegate void UpdateFileredCombinedListviewDelegate(ListView list, string filter);
+        delegate string GetTextBoxTextDelegate(TextBox txBox);
+        delegate string GetLisviewSubitem0Delagate(ListView list, int i);
+
+
+        public string GetLisviewSubitem0(ListView list,int i)
+        {
+
+            if (list.InvokeRequired)
+            {
+
+                return (string)list.Invoke(new GetLisviewSubitem0Delagate(GetLisviewSubitem0), new object[] { list, i });
+
+            }
+            else
+            {
+                return list.Items[i].SubItems[0].Text;
+            }
+        }
+
+        public string GetTextBoxText(TextBox txBox)
+        {
+
+            //if (this.listView1.InvokeRequired)
+            //    return (string)this.listView1.Invoke(new GetListText(GetText), new object[] { i, j });
+            //else
+            //{
+            //    return listView1.Items[i].SubItems[j].Text;
+
+            //}
+
+            if (txBox.InvokeRequired)
+            {
+               
+                    return (string)txBox.Invoke(new GetTextBoxTextDelegate(GetTextBoxText), new object[] { txBox });
+               
+            }
+            else
+            {
+                return txBox.Text;
+            }
+        }
+
+
+        public void UpdateFileredCombinedListview( ListView list, string filter)
+        {
+
+            if (list.InvokeRequired)
+                try
+                {
+                    list.Invoke(new UpdateFileredCombinedListviewDelegate(UpdateFileredCombinedListview), new object[] {  list, filter });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("UpdateListview  " + ex.ToString());
+                }
+            else
+            {
+                if (filter.Length > 0)
+                {
+                    string[] str = new string[2];
+
+
+                    label1.Text = WndTimeList.Count.ToString();
+                    TimeSpan sTime = new TimeSpan();
+                    for (int i = 0; i <= WndTimeList.Count - 1; i++)
+                    {
+                        if (WndTimeList[i].getWindowName().Contains(filter))
+                        {
+                            sTime += WndTimeList[i].getTimeElapsed();
+                        }
+                    }
+
+                    if (sTime.ToString().Length > 8)
+                    {
+                        str[0] = sTime.ToString().Remove(8);
+                    }
+                    str[1] = filter;
+                    ListViewItem itm = new ListViewItem(str);
+
+                    listView1.Items.Clear();
+
+                    this.listView1.Items.Add(itm);
+
+                }
+                
+            }
+
+ 
+
+        }
+
+        public void UpdateFileredListview(String window, String time, ListView list,string filter)
+        {
+
+            if (list.InvokeRequired)
+                try
+                {
+                    list.Invoke(new UpdateFileredListviewDelegate(UpdateFileredListview), new object[] { window, time, list, filter });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("UpdateListview  " + ex.ToString());
+                }
+            else
+            {
+                string[] str = new string[2];
+                str[0] = time;
+                str[1] = window;
+                ListViewItem itm = new ListViewItem(str);
+                removeNotExisting();
+                label1.Text = WndTimeList.Count.ToString();
+               // bool exists = false;
+
+
+                for (int i = 0; i <= listView1.Items.Count - 1; i++)
+                {
+
+                    if (this.listView1.Items[i].SubItems[1].Text == window &&
+                        this.listView1.Items[i].SubItems[1].Text.Contains(filter))// already exists in listview
+                    {
+
+                       // exists = true;
+                        this.listView1.Items[i].SubItems[1].Text = window;
+                        this.listView1.Items[i].SubItems[0].Text = time;
+
+
+                        listView1.Sort();
+                      //  TimeSpan timeSumm = new TimeSpan();
+                    
+                      
+                     
+
+                    }
+                    else
+                        if (this.listView1.Items[i].SubItems[0].Text.CompareTo("00:00:01") < 0 || !this.listView1.Items[i].SubItems[1].Text.Contains(filter))
+                        {
+                            this.listView1.Items[i].Remove();
+                        }
+                  
+
+
+                }
+                if (//exists == false &&
+                    window != null && 
+                    window.Contains(filter)&&
+                    searchListbox(window)<0)
+                {
+                    this.listView1.Items.Add(itm);
+
+
+                }
+
+
+            }
+ 
+        }
+        public void UpdateListview(String window, String time,ListView list)
+        {
+            if (list.InvokeRequired)
+                try
+                {
+                    list.Invoke(new UpdateListviewDelegate(UpdateListview), new object[] { window, time, list });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("UpdateListview  " + ex.ToString());
+                }
+            else
+            {
+                string[] str = new string[2];
+                str[0] = time;
+                str[1] = window;
+                ListViewItem itm = new ListViewItem(str);
+                removeNotExisting();
+                label1.Text = WndTimeList.Count.ToString();
+                bool exists = false;
+                for (int i = 0; i <= listView1.Items.Count - 1; i++)
+                {
+                    
+                    if (this.listView1.Items[i].SubItems[1].Text == window)
+                    {
+
+                        exists = true;
+                        this.listView1.Items[i].SubItems[1].Text = window;
+                        this.listView1.Items[i].SubItems[0].Text = time;
+
+                      
+                        listView1.Sort();
+
+
+                    }
+                    else
+                        if (this.listView1.Items[i].SubItems[0].Text.CompareTo("00:00:01") < 0)
+                        {
+                            this.listView1.Items[i].Remove();
+                        }
+
+
+
+
+                }
+                if (exists == false && window != null)
+                {
+                    this.listView1.Items.Add(itm);
+
+
+                }
+               
+
+            }
+        }
+
+
+        public void UpdateLabel(String str, Label la)
+        {
+
+            if (la.InvokeRequired)
+                try
+                {
+                    la.Invoke(new UpdateLabelDelegate(UpdateLabel), new object[] { str, la });
+                }
+                catch (Exception)
+                { }
+            else
+                la.Text = str;//.AppendText(msg);
+
+        }
+
+      
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+
+        Thread LoopThread;
+        Stopwatch watch = new Stopwatch();
+
+        //WndTime[] WndTimeArray = new WndTime[200];
+        List<WndTime> WndTimeList = new List<WndTime>();
+        int count = 0;
+
+        bool running = false;
+                                    
+        int searchListbox(string windowName)
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].SubItems[1].Text == windowName)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        int searchList(string windowName)
+        {
+            for (int i = 0; i < WndTimeList.Count; i++)
+            {
+                if (WndTimeList[i].getWindowName() == windowName)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        void removeNotExisting()
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (searchList(listView1.Items[i].SubItems[1].Text)<0)
+                {
+                    listView1.Items[i].Remove();
+                }
+            }
+        }
+
+        //void removeNotExisting()
+        //{
+        //    for (int i = 0; i < WndTimeList.Count; i++)
+        //    {
+        //         if(searchListbox(WndTimeList[i].getWindowName())<0)
+        //         {
+        //             WndTimeList.RemoveAt(i);
+        //         }
+        //    }
+        //}
+
+
+        void stopAll()
+        {
+            for (int i = 0; i < WndTimeList.Count; i++)
+            {
+                WndTimeList[i].stopWatch();
+            }
+        }
+
+
+        void WindowsStatisticTime()
+        {
+            while (true)
+            {
+                String caption = GetActiveWindowTitle();
+
+
+                string filter = GetTextBoxText(textBox_filter);
+                int indexof = searchList(caption);
+
+
+                //for (int i = 0; WndTimeList.Count > i; i++)
+                //{
+                //    int ind = searchList(WndTimeList[i].getWindowName());
+                //    UpdateFileredListview(WndTimeList[ind].getWindowName(),
+                //        WndTimeList[ind].getTime(),
+                //        listView1, filter);
+
+                //}
+
+                if (filter.Length > 0)
+                {
+                    UpdateFileredCombinedListview(listView1, filter);
+
+
+                }
+                else
+
+
+                {
+
+                    for (int i = 0; WndTimeList.Count > i; i++)
+                    {
+                        int ind = searchList(WndTimeList[i].getWindowName());
+                        UpdateFileredListview(WndTimeList[ind].getWindowName(),
+                            WndTimeList[ind].getTime(),
+                            listView1, filter);
+
+                    }
+                }
+                    TimeSpan summTime = new TimeSpan();
+
+                    for (int i = 0; listView1.Items.Count > i; i++)
+                    {
+                        TimeSpan tmSpan = new TimeSpan();
+
+
+                        TimeSpan.TryParse(GetLisviewSubitem0(listView1, i), out tmSpan);
+
+                        summTime += tmSpan;
+                        //   tmSpan=listView1.Items[i].SubItems[0];
+                    }
+
+                    if (summTime.ToString().Length >= 8)
+                    {
+                        UpdateLabel(summTime.ToString(), label_timeSummary);
+                    }
+
+                    // UpdateLabel(summTime.ToString().Remove(8), label_timeSummary);
+
+                    if (indexof > -1)// if found update time
+                    {
+                        stopAll();
+                        WndTimeList[indexof].startWatch();
+
+                        // TimeSpan timeSumm = new TimeSpan();
+                        TimeSpan time = WndTimeList[indexof].getTimeElapsed();
+                        String TimeStr = time.ToString();
+                        if (time.ToString().Length > 8)
+                        {
+                            WndTimeList[indexof].setTime(TimeStr.Remove(8));
+                        }
+
+                    //    UpdateListview(WndTimeList[indexof].getWindowName(),
+                         //   WndTimeList[indexof].getTime(), listView1);
+
+                        //UpdateFileredListview(WndTimeList[indexof].getWindowName(),
+                        //    WndTimeList[indexof].getTime(),
+                        //    listView1, filter);
+
+
+                        //////////////////
+                        //timeSumm += WndTimeList[indexof].getTimeElapsed();
+
+
+                        //if (timeSumm.ToString().ToString().Length > 8)
+                        //{
+                        //    UpdateLabel(timeSumm.ToString().Remove(8), label_timeSummary);
+                        //}
+
+
+                    }
+                    else //else add  new entry
+                    {
+                        if (caption != null)
+                        {
+
+
+                            //  UpdateFileredListview(caption, "00:00:00", listView1,filter);
+                         //   UpdateListview(caption, "00:00:00", listView1);
+                            WndTimeList.Add(new WndTime("00:00:00", caption));
+
+                            //WndTimeList[count].setWindowName(caption);
+                            count++;
+
+
+                        }
+                    }
+                
+
+              // UpdateFileredCombinedListview(listView1, filter);
+               // UpdateListview(caption, "0",listView1);
+                Thread.Sleep(900);
+            }
+        }
+
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    WndTimeArray[i] = new WndTime();
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            LoopThread = new Thread(WindowsStatisticTime);
+            LoopThread.IsBackground = true;
+            LoopThread.Start();
+            running = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            watch.Reset();
+
+          //  UpdateLabel(watch.Elapsed.ToString(), this.label1);
+        }
+
+
+        class WndTime
+        {
+            private String windowName;
+            private String Time;
+
+            private Stopwatch watch = new Stopwatch();
+
+            public void startWatch()
+            {
+                this.watch.Start();
+            }
+
+            public void stopWatch()
+            {
+                this.watch.Stop();
+            }
+
+            public void resetWatch()
+            {
+                this.watch.Reset();
+            }
+
+
+            public TimeSpan getTimeElapsed()
+            {
+              return  this.watch.Elapsed;
+            }
+
+            public WndTime()
+            {
+                setTime("00:00:00");
+                setWindowName("noname");
+            }
+
+            public WndTime(string time, string caption)
+            {
+                setTime(time);
+                setWindowName(caption);
+            }
+            public void setTime(string str)
+            {
+                this.Time = str;
+            }
+
+           public void setWindowName(string str)
+            {
+                this.windowName = str;
+            }
+
+           public String getTime()
+            {
+                return this.Time;
+            }
+
+           public String getWindowName()
+            {
+                return this.windowName;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (running == true)
+            {
+                try
+                {
+                    LoopThread.Suspend();
+                    this.button1.Text = "Resume";
+                    running = false;
+                    stopAll();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else if (running== false)
+            {
+                try
+                {
+                    LoopThread.Resume();
+                    this.button1.Text = "Pause";
+                    running = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+        }
+    }
+}
